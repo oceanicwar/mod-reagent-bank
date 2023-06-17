1df5a76a3d5cbae41ac8a455d335e37dd25b6144
jshplayer <jshplayer@outlook.com>
2023-06-18 11:11:56 +1200

    Query Adjust

--- data/sql/db-world/base/reagent_bank_NPC.sql
+++ data/sql/db-world/base/reagent_bank_NPC.sql
@@ -1,4 +1,3 @@
-
 SET
 @Entry = 190011,
 @Name = "Oceanic War Reagent Banker";
@@ -6,4 +5,4 @@ SET
 DELETE FROM `creature_template` WHERE `entry` = @Entry;
 
 INSERT INTO `creature_template` (`entry`, `modelid1`, `modelid2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `scale`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, 371, 0, @Name, NULL, NULL, 0, 6, 6, 0, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker');
+(@Entry, 371, 0, @Name, NULL, NULL, 0, 80, 80, 0, 35, 1, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker');
